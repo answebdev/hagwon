38526eb01565b0c46af8d99a97a667e1531e87ce
ANS <36783010+answebdev@users.noreply.github.com>
2022-05-23 15:18:23 -0700

    add React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
 import './App.css';
 
 // Integrating Google Maps with React: https://blog.logrocket.com/integrating-google-maps-react/
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1>ABC English</h1>
-        <p>Coming Soon...</p>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        {/* <Route exact path='/:slug' element={<Course />} /> */}
+        {/* <Route path='*' element={<Error />} /> */}
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
